214ef74ac8b1d7e6a1e9dc079b44e319b8ba3558
Samuel Garcia <76258324+GarcoPro23@users.noreply.github.com>
2023-09-11 21:15:15 -0500

    Add files via upload

---
+++ MisPrimerosCodigos/Tree.java
@@ -0,0 +1,15 @@
+class Tree  
+{    
+    public static void main(String args[]) {  
+       System.out.println("Hola , Soy Samuel y estoy aprendiendo a codificar en Java!");
+       System.out.println("Voy a plantar un arbol hoy!” o “Listo para ensuciarme las manos!");
+       System.out.println("     *   ");
+       System.out.println("    ***  ");
+       System.out.println("  ******* ");
+       System.out.println(" ********* ");
+       System.out.println("***********");
+       System.out.println("     *  ");
+       System.out.println("     *  ");
+       System.out.println("     *  ");
+   }  
+}  
---
+++ MisPrimerosCodigos/compoundOperatorsExample.java
@@ -0,0 +1,18 @@
+//Compound operators:
+/*
+Addition(+=)
+Subtration(-=)
+Multiplication (*=)
+Division (/=)
+Modulo(%=)
+*/
+public class BakeSale {
+	public static void main(String[]args) {
+		int numCookies=17;
+		numCookies-= 3;
+		System.out.println(numCookies);
+		numCookies /=2;
+		//Add your code above
+		System.out.println(numCookies);
+		}
+}
