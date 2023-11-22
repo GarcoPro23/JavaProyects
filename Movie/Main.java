d2c1bd6d8dc0b014dd7a295ee51f3c65b9f3b510
Samuel Garcia <76258324+GarcoPro23@users.noreply.github.com>
2023-11-21 21:06:56 -0500

    Create Main.java

---
+++ Movie/Main.java
@@ -0,0 +1,14 @@
+package movie;
+
+//import java.util.ArrayList;
+//import movie.ui.UIPeliculas;
+
+import javax.swing.JOptionPane; 
+import movie.ui.UIPeliculas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UIPeliculas.interfazUsuario();                        
+    }    
+}
